0d0113101762dceffd285fa43a5109b27d1bc8cd
jiangjj <149521920@qq.com>
2018-10-20 21:39:24 +0800

    update

--- employee/src/main/java/com/jiangjj/employee/EmployeeApplication.java
+++ employee/src/main/java/com/jiangjj/employee/EmployeeApplication.java
@@ -3,10 +3,11 @@ package com.jiangjj.employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@@SpringBootApplication
+@SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableMongoRepositories
---
+++ gateway/src/main/java/com/jiangjj/gateway/GatewayApi.java
@@ -0,0 +1,35 @@
+package com.jiangjj.gateway;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class GatewayApi {
+    @Autowired
+    ZuulProperties properties;
+    @Primary
+    @Bean
+    public SwaggerResourcesProvider swaggerResourcesProvider() {
+        return () -> {
+            List resources = new ArrayList();
+            properties.getRoutes().values().stream()
+                    .forEach(route -> resources.add(createResource(route.getId(), "2.0")));
+            return resources;
+        };
+    }
+    private SwaggerResource createResource(String location, String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(location);
+        swaggerResource.setLocation("/" + location + "/v2/api-docs");
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+}
